b5a2d754bafecd2b1dcdc931a1d53f47f821d3c4
Esteban-Garcia-Gonzalez <89358442+Esteban-Garcia-Gonzalez@users.noreply.github.com>
2021-09-07 09:55:07 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
+    p1=0
+    p2=0
+    prom=0
+    c=0
+    while p1>=0:
+        p1=float(input())
+        c=c+1
+        p2=p1+p2
+        if 0>p1:
+            p2=p2-p1
+            c=c-1
+            prom=p2/c
+    print(prom)
     pass
 if __name__=='__main__':
     main()
